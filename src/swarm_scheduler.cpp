ec4aa663aceef055cd75133d43eb3e26168365a4
akash-k-s <akash2001ks@gmail.com>
2023-08-18 17:27:52 +0200

    added drone queue

--- src/swarm_scheduler.cpp
+++ src/swarm_scheduler.cpp
@@ -2,6 +2,11 @@
 #include<stdio.h>
 #include<vector>
 #include<cmath>
+#include <map>
+#include <string.h>
+#include "Eigen/Core"
+#include "Eigen/Dense"
+#include "include/swarm_planner_deps/swarm_config_tracker.hpp"
 
 class mission{
     private:
@@ -10,6 +15,11 @@ class mission{
     int drones_len;
     std::vector<std::vector<int>> drone_mission;
     std::vector <int> status;
+    std::map<std::string, std::vector<int>> drone_planner;
+    std::map<std::string, std::vector<int>> mission_logger;
+    std::vector<std::string> drones_list;
+    std::vector<Eigen::Vector4d> drone_states;
+
     public:
 
     int get_mission_len(){
@@ -32,6 +42,17 @@ class mission{
         return status;
     }
 
+    std::map<std::string, std::vector<int>> getdrone_planner(void){
+        return drone_planner;
+    }
+    
+    std::map<std::string, std::vector<int>> getmission_logger(void){
+        return mission_logger;
+    }
+
+    std::vector<std::string> getdrones(void){
+        return drones_list;
+    }
     //set functions
 
     void setmissions_len(int x){
@@ -57,16 +78,159 @@ class mission{
             printf("th matrix size is not same as drone lenght and payload len");
         }
     }
+     
+    void setdrones(const std::vector<std::string> drone_){
+        if(drone_.size() == drones_len ){
+            drones_list = drone_;
+        }
+        else{
+            printf("the drone list lenght is not same as drone lenght");
+        }
+    }
+
 
     void createstatus_list(){
         status.clear();
         for(int i=0 ; i<mission_len; i++){
             status.push_back(0);
             //status = 0 not started
-            //status = 1 started 
-            //status = 2 queued
-            //status = 3 compeled
+            //status = 1 pickup done  
+            //status = 2 drop off done 
         }
+    }
+    
+    
+
+    //other methods
+
+    void missions(void){
+        std::string drone;
+        std::vector<int> data;
+        int payload; 
+        int x;
+        std::vector<int> temp;
+        //std::vector<std::string> current_mission_drones;
+        data.push_back(-1);
+        for(int i = 0; i<drones_len;i++){
+            drone = drones_list[i];
+            drone_planner[drone] = data;
+        }
+        for(int i =0; i<mission_len;i++){
+            payload = mission_idx[i];
+            drone_mission[payload] = data;
+        }  
+        for(int i=0; i<mission_len;i++){
+            //current_mission_drones.clear();
+            for(int j=0; j<drones_len;j++){
+                if(drone_mission[i][j]==1){
+                    x = mission_idx[i]; //
+                    drone = drones_list[j];
+                    temp = drone_planner[drone];
+                    if (temp[0] == -1){
+                        temp.clear();
+                        temp.push_back(x);
+                    }
+                    else{
+                        temp.push_back(x);
+                    }
+                    drone_planner[drone] = temp;
+                }
+            }
+        } 
+    }
+
+    void mission_check(void){
+        int len_ = mission_len; //payload lenght
+        int drones_len_ = drones_len; // drones lenght
+        std::string drone_;
+        std::vector<int> temp;
+        int drone_current_mission;
+        int check = 1;
+        int reach;
+        float final_x,final_y,current_x,current_y;
+        for(int i=0 ;i<drones_len_;i++){
+            drone_ = drones_list[i];
+            temp = drone_planner[drone_];
+            if(temp.size()==0){
+                continue;
+            }
+            else{
+                drone_states = swarm_config_tracker->read_drone_states();
+                drone_current_mission = temp[0];
+                if (status[drone_current_mission] == 0){
+                    check = 1;
+                    //goto pickup point
+                    for(int j=0; j<drones_len_;j++){
+                        if (drone_mission[drone_current_mission][j]==1){
+                            current_x = drone_states[j][0];
+                            current_y = drone_states[j][1];
+
+                            //check if all reached
+                            check = check *reach;
+                        }
+                    }
+                    if(check == 1){
+                        status[drone_current_mission]=1 
+                        //change raduis 
+                    }
+                    
+                    
+                }
+                else if (status[drone_current_mission]==1){
+                    //goto drop off
+                    //check if any drone is reached 
+                    //update status = 2 
+                    //pop mission from the drones in mission
+                }
+
+                
+                
+
+                    for(int j=0; j<drones_len;j++){
+                        drone_mission
+                    }
+                
+            }
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            if (status[i]==0){
+                int check = 1;
+                for (int j =0 ; j<drones_len_; j++){
+                    if(drone_mission[i][j]==1){
+                        //check if the drones have reached postion
+                        check = check*1;
+                        if(check == 1 ){
+                            this -> mission_start();
+                            status[i] = 1;
+                        }
+                    }
+
+                }
+            }
+            else if (status[i]==1){
+                for(int j =0; j<drones_len_;j++){
+                    if (drone_mission[i][j]==1){
+                        //
+                    }
+                }
+            }
+        }
+    }
+
+    void mission_start(){
+        //start
     }
 
 };
