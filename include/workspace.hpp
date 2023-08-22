eedf5a77c8d15588b9a44def3ea7cccd68b52202
DS3a <33351122+DS3a@users.noreply.github.com>
2023-08-22 14:04:33 +0200

    clean code

--- include/workspace.hpp
+++ include/workspace.hpp
@@ -13,14 +13,6 @@
 #include "math.h"
 
 
-// helper type for the visitor #4
-template<class... Ts>
-struct overloaded : Ts... { using Ts::operator()...; };
-
-// explicit deduction guide (not needed as of C++20)
-template<class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
-
 
 namespace mtsp_drones_gym {
     class Workspace {
