1d2099677123b95f3b5ecae5dd00e475676cb6f2
mshashank02 <mshashank02@gmail.com>
2023-08-22 12:30:51 +0200

    setpoints working

--- include/path_follow.hpp
+++ include/path_follow.hpp
@@ -6,43 +6,68 @@
 #include "Eigen/Core"
 #include "Eigen/Dense"
 
+#define kp 1
 
+namespace path_follow {
 
-std::vector<Eigen::Vector2d> get_drone_velocity_setpoint();
-std::vector<Eigen::Vector2d> get_drone_setpoint_error();
-
-std::vector<Eigen::Vector2d> drone_velocity_setpoint;
-std::vector<Eigen::Vector2d> drone_setpoint_error;
-int kp = 1;
+std::vector<Eigen::Vector2d> get_drone_velocity_setpoint(std::vector<Eigen::Vector4d> current_drone_state, std::vector<std::vector<Eigen::Vector2d>> path,std::vector<bool> found);
 
 
 
 
 
 
-std::vector<Eigen::Vector2d> get_drone_velocity_setpoint(std::vector<Eigen::Vector4d> current_drone_state, std::vector<std::vector<Eigen::Vector2d>> path){
-    for (int i =0; i<= path.size(); i++){
-        
-        
-
-        double current_drone_state_x = current_drone_state[i][0];
-        double current_drone_state_y = current_drone_state[i][1];
-        double path_x = path[i][0][0];
-        double path_y = path[i][0][1];
-        double drone_setpoint_error_x = path_x - current_drone_state_x;
-        double drone_setpoint_error_y = path_y - current_drone_state_y;
-        drone_setpoint_error[i](0) = drone_setpoint_error_x;
-        drone_setpoint_error[i](1) = drone_setpoint_error_y;
-        drone_velocity_setpoint[i] = kp*drone_setpoint_error[i] ;
-        printf("hi");
 
 
+std::vector<Eigen::Vector2d> get_drone_velocity_setpoint(std::vector<Eigen::Vector4d> current_drone_state, std::vector<std::vector<Eigen::Vector2d>> path,std::vector<bool> found){
+    std::cout<<"in get_drone_velocuty\n";
+    static std::vector<Eigen::Vector2d> drone_velocity_setpoint(path.size());
+    std::cout<<"got drone vel\n";
+    static std::vector<Eigen::Vector2d> drone_setpoint_error(path.size());
+    std::cout<<"got setpoint error\n";
+    
+    for (int i =0; i< path.size(); i++){
+        std::cout << "in for loop; iteration " << i <<std::endl;
+        if (found[i]){
+            double current_drone_state_x = current_drone_state[i][0];
+            double current_drone_state_y = current_drone_state[i][1];
+            std::cout<<"inside got states\n";
+            double path_x;
+            double path_y;
+            if (path[i].size() > 0){
+                std::cout<<"okay size"<<i;
+                double path_x = path[i][1][0];
+                double path_y = path[i][1][1];
 
-            
-        
+            }
+            else {
+                std::cout<<"not okay size";
+                double path_x = path[i][0][0];
+                double path_y = path[i][0][1];
+            }
+            double drone_setpoint_error_x = path_x - current_drone_state_x;
+            double drone_setpoint_error_y = path_y - current_drone_state_y;
+            std::cout<<"setpoint error:"<<drone_setpoint_error_x<<","<<drone_setpoint_error_y<<std::endl;
+            drone_setpoint_error[i][0] = drone_setpoint_error_x;
+            drone_setpoint_error[i][1] = drone_setpoint_error_y;
+            std::cout<<"added errors insidn\n";
+            drone_velocity_setpoint[i] = kp*drone_setpoint_error[i] ;
+            std::cout<<"got setpoints:"<<drone_velocity_setpoint[i]<<std::endl;
+
+        }
+        else{
+            continue;
+        }    
     }
+
+    
+
+
+    
+
+    return drone_velocity_setpoint;
 }
 
-
+}
 
 #endif
--- src/mtsp_environment.cpp
+++ src/mtsp_environment.cpp
@@ -9,7 +9,7 @@
 #include<string>
 int main() {
     mtsp_drones_gym::Workspace ws(true);
-    ws.add_drone(0, 0, 0.2, 1);
+    ws.add_drone(0, 1.5, 0.2, 1);
     ws.add_drone(0, 1, 0.1, 1);
     ws.add_drone(0, -1, 0.1, 1);
     ws.add_drone(-1, -1, 0.1, 1);
@@ -38,7 +38,7 @@ int main() {
     swarm_config_tracker->set_num_drones(5);
 
     swarm_config_tracker->write_swarm_config(std::vector<Eigen::Vector4d> {
-        Eigen::Vector4d(0, 0, 0.1, 0),
+        Eigen::Vector4d(0, 1.50, 0.1, 0),
         Eigen::Vector4d(0, 1, 0.1, 1),
         Eigen::Vector4d(0, -1, 0.1, 0),
         Eigen::Vector4d(1, -1, 0.1, 0),
@@ -55,40 +55,51 @@ int main() {
 
     ws.set_swarm_config_tracker(swarm_config_tracker);
     for (int i=0; i<100; i++) {
-        std::cout << "STEP " << i << std::endl;
+        std::cout << "Entered For loop " << i << std::endl;
         auto output = ws.step();
-        std::cout << "STEP " << i << std::endl;
+        std::cout << "STEP" << i << std::endl;
         std::vector<Eigen::Vector4d> drone_states = std::get<1>(output);
         std::cout << "STEP " << i << std::endl;
         swarm_config_tracker->write_swarm_config(drone_states, goals);
-        std::cout << "STEP " << i << std::endl;
+        std::cout << "writing goals " << i << std::endl;
         planner.plan_paths();
-        std::cout << "STEP " << i << std::endl;
+        std::cout << "Planned paths" << i << std::endl;
         // planner.write_states_and_goals(drone_states, goals);
-        std::cout << "STEP " << i << std::endl;
+        
         std::vector<bool> paths_found;
-        std::cout << "STEP " << i << std::endl;
+        std::cout << "paths found " << i << std::endl;
         std::vector<std::vector<Eigen::Vector2d>> paths;
-        std::cout << "STEP " << i << std::endl;
+        std::cout << "paths " << i << std::endl;
 
         std::tie(paths_found, paths) = planner.get_paths();
-        std::cout << "STEP " << i << std::endl;
+        std::cout << "got paths" << i << std::endl;
         drone_states = swarm_config_tracker->read_drone_states();
         
-        std::cout << "STEP " << i << std::endl;
-        ws.draw_paths(paths);
-        std::cout << "STEP " << i << std::endl;
+        std::cout << "read drone states" << i << std::endl;
+        ws.draw_paths(paths, paths_found);
+        std::cout << "drew paths " << i << std::endl;
+        
+        std::cout<<paths_found.size()<<"is drone states size"<<std::endl;
 
-        std::vector<Eigen::Vector2d> drone_setpoints = get_drone_velocity_setpoint(drone_states, paths);
+        std::vector<Eigen::Vector2d> drone_setpoints = path_follow::get_drone_velocity_setpoint(drone_states, paths,paths_found);
               
-        std::cout << "STEP " << i << std::endl;
 
-        for (int i=0; i < drone_setpoints.size(); i++) {
-            drone_list.push_back((mtsp_drones_gym::Move) {.x = drone_setpoints[i][0],.y = drone_setpoints[i][1]});
+        std::cout << "got setpoint" << i << std::endl;
+        
+
+        for (int j=0; j < paths.size(); j++) {
+            std::cout << "path x: " << paths[j][1][0] << std::endl;
+            std::cout << "path y: " << paths[j][1][1] << std::endl;
+            std::cout << "current x: " << drone_states[j][0] << std::endl;
+            std::cout << "current y: " << drone_states[j][1] << std::endl;
+            std::cout << "drone setpoints are : " << drone_setpoints[j] << std::endl;
+            
+
+            drone_list.push_back((mtsp_drones_gym::Move) {.x = drone_setpoints[j][0],.y = drone_setpoints[j][1]});
         }
         
-        std::cout << "STEP " << i << std::endl;
-        // ws.set_actions(drone_list);
+        std::cout << "gave setpoint " << i << std::endl;
+        ws.set_actions(drone_list);
 
      }
 
